2d234dbd30f6b176632168fa95dc007fbb84f151
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-09 10:10:43 -0700

    download d3, create test bar graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import * as d3 from 'd3'
 
 class LineGraph extends Component {
+  drawGraph() {
+    const data = [12, 5, 8, 15, 3, 9]
+
+    const svg = d3.select('svg').attr("width", 700).attr("height", 300)
+
+    svg.selectAll("rect").data(data).enter().append("rect")
+      .attr("x", (d, i) => i * 70)
+      .attr("y", 0)
+      .attr("width", 35)
+      .attr("height", (d, i) => 300 - d * 10)
+      .attr("fill", "blue")
+
+  }
+  componentDidMount() {
+    this.drawGraph()
+  }
   render() {
     return (
-      <h3>Line Graph</h3>
+      <div>
+        <h3>Line Graph</h3>
+        <svg></svg>
+      </div>
     )
   }
 }
